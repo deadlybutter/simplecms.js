ab3c4782351e08366a05b18812e3a8114372dbda
Joe Kent <thedeadlybutter@gmail.com>
2016-04-23 13:29:16 -0400

    Saves express vars in router module

--- router.js
+++ router.js
@@ -1,10 +1,16 @@
+var app;
+var express;
+
 function implementStructurePath(structure) {
   app.get(structure.url_prefix, function(req, res) {
     res.json(structure);
   });
 }
 
-module.exports = function(app, express) {
+module.exports = function(a, e) {
+  app = a;
+  express = e;
+  
   var module = {};
 
   module.path = implementStructurePath;
