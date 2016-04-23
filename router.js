1d21e306dba0fe6e2f904a88d1ae18c751d33353
Joe Kent <thedeadlybutter@gmail.com>
2016-04-23 14:00:01 -0400

    Changes prefix to path

--- router.js
+++ router.js
@@ -10,7 +10,7 @@ function implementStructurePath(structure) {
 function customRouter(path, router_module, application) {
   var router = express.Router();
   router_module(router);
-  app.use(prefix, router);
+  app.use(path, router);
 }
 
 module.exports = function(a, e) {
