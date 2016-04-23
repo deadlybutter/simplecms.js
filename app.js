8f738201af683ab5ef2344713f48c251fff73848
Joe Kent <thedeadlybutter@gmail.com>
2016-04-23 12:52:54 -0400

    REWRITE into node module

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 var express = require('express');
 var app = express();
-
 var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main',
-  partialsDir: [__dirname + '/views/partials'],
-  helpers: {}
-}));
-app.set('view engine', 'handlebars');
 
-function startServer() {
+function startServer(directory) {
+  if (!directory) {
+    directory = __dirname + "/../../";
+  }
+
+  app.engine('handlebars', exphbs({
+    defaultLayout: 'main',
+    partialsDir: [directory + '/views/partials'],
+    helpers: {}
+  }));
+  app.set('view engine', 'handlebars');
+
   var PORT = process.env.PORT || 5000;
   app.listen(PORT, function () {
     console.info(`simplecms.js listening on port ${PORT}`);
   });
 }
 
-this.modules = {};
-require(__dirname + '/simple_modules/_loader')(app, express, function modulesLoaded(mods) {
-  this.modules = mods;
-  startServer();
-})
+module.exports = function() {
+  start: startServer
+}
